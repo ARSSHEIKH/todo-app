a10da66b9192b11a0aa67089f79c7d3624daeb0e
Arsalan <arsheikh665@gmail.com>
2021-02-23 20:26:10 +0500

    updated edit part

---
+++ todo-app-js-v1.0.1/web-app/app/index.js
@@ -0,0 +1,112 @@
+let countList = 0
+let lists = document.getElementById("listItems")
+
+function createNodes(text, date, countList) {
+
+    //#region To create elements
+    //#region initializing variables to create elements
+    let inpEdit = document.createElement("textarea");
+    let editSubmit = document.createElement("button");
+    let editSubmitText = document.createTextNode("Submit");
+    
+    let trMain = document.createElement("tr");
+    let tr = document.createElement("tr");
+    let tdId = document.createElement("th");
+    let td2 = document.createElement("td");
+    let tdDate = document.createElement("td");
+    let trDelete = document.createElement("tr");
+    let tdDelete = document.createElement("td");
+    let btnDelete = document.createElement("button");
+    let tdEdit = document.createElement("td");
+    let btnEdit = document.createElement("button");
+
+    //#endregion
+
+    //#region setting attributes
+    tdId.setAttribute("scope", "row")
+    tdId.setAttribute("id", "serialNo")
+    td2.setAttribute("class", "td2Value")
+    tdDate.setAttribute("class", "tdDate")
+    //#endregion
+
+    //#region setting attributes for editting
+    inpEdit.setAttribute("type", "text")
+    inpEdit.setAttribute("id", "inpEdit")
+    inpEdit.setAttribute("disabled", "true")
+    inpEdit.setAttribute("class", "form-control")
+    editSubmit.setAttribute("id", "inpEditBtn")
+    editSubmit.setAttribute("disabled", "true")
+    editSubmit.setAttribute("onclick", "editSubmit(this)")
+    editSubmit.setAttribute("class", "btn btn-success btn-sm")
+    btnEdit.setAttribute("onclick", "editItem(this)")
+    btnEdit.setAttribute("class", "btnEdit btn btn-outline-success")
+    //#endregion
+    
+    //#region setting attributes for Deleting
+    trDelete.setAttribute("class", "trDelete")
+    btnDelete.setAttribute("class", "btnDelete btn btn-outline-danger")
+    btnDelete.setAttribute("onclick", "deleteItem(this)")
+   
+    let btnDeleteText = document.createTextNode("Delete");
+    let btnEditText = document.createTextNode("Edit");
+    //#endregion
+
+    //#region appending to Childs Element
+    tr.appendChild(tdId)
+    tr.appendChild(td2)
+    lists.appendChild(trMain)
+    trMain.appendChild(tr)
+    trMain.appendChild(trDelete)
+
+    trDelete.appendChild(tdEdit)
+    tdEdit.appendChild(btnEdit)
+    btnEdit.appendChild(btnEditText)
+
+    trDelete.appendChild(tdDelete)
+    tdDelete.appendChild(btnDelete)
+    btnDelete.appendChild(btnDeleteText)
+
+    td2.appendChild(inpEdit)
+    td2.appendChild(editSubmit)
+    td2.appendChild(tdDate)
+    editSubmit.appendChild(editSubmitText)
+
+    tdId.appendChild(document.createTextNode(countList));
+    inpEdit.textContent = text;
+    tdDate.appendChild(document.createTextNode(date));
+
+    //#endregion
+    //#endregion
+}
+function AddList() {
+    let text = document.getElementById("inpText");
+    let date = new Date()
+    createNodes(text.value, date, ++countList);
+}
+function editItem(param) {
+    let editText = param.parentNode.parentNode.parentNode;
+    let inpText = editText.querySelector("#inpEdit")
+    let inpEditBtn = editText.querySelector("#inpEditBtn")
+    inpText.style.background = "#fff"
+    inpText.disabled = false
+    inpEditBtn.disabled = false
+}
+function editSubmit(param) {
+    let parentTd = param.parentNode;
+    let inpEdit = parentTd.querySelector("#inpEdit")
+    let serialNo = parentTd.parentNode.querySelector("#serialNo").textContent
+    let inpEditBtn = parentTd.querySelector("#inpEditBtn")
+    inpEdit.style.background = "rgb(63, 128, 158)"
+    inpEdit.focus = true
+    inpEdit.disabled = true
+    inpEditBtn.disabled = true
+}
+function deleteItem(param) {
+    let parentTd = param.parentNode.parentNode.parentNode;
+    let serialNo = parentTd.querySelector("#serialNo").textContent
+    parentTd.remove();
+}
+function deleteAll() {
+    lists.innerHTML = ""
+    countList = 0
+}
--- todo-react-app/src/todoList.js
+++ todo-react-app/src/todoList.js
@@ -1,40 +1,70 @@
-import React from 'react'
-let list = []
-let ind = 0
+import React, { useEffect } from 'react'
 
+let ind = 0
 const TodoList = (props) => {
+    let elemDisabler = true
     return (
         <div className="dvTable-list col">
+
+            <button className="btnDeleteAll"
+                onClick={() => {
+                    document.querySelector(".container").innerHTML = null
+                }}
+            >Delete All</button>
+
             <h2 align="center">Lists</h2>
             <div className="container">
-                {props.title.map((text,  key) => {
-                ind = key
+                {props.title.map((text, key) => {
+                    ind = key
                     if (ind % 2 == 0) {
-                        ind = "even"
+                        ind = "even";
                     }
                     else {
                         ind = "odd"
                     }
-
                     return (
-                        <div className="dvMain">
-                        <div className={ind}>
-                            <div className="tbl-List col" colspan="3">
-                                <div className="dvTitle">{text}</div>
-                               
-                                <div className="btnDeleteTitle" onClick={DeleteSelected(key)}>X</div>
+                        <div className="dvMain" key={key}>
+                            <div className={ind} id={key}>
+                                <div className="tbl-List col" colspan="3">
+                                    <input type="text" className="dvTitle" defaultValue={text} disabled={elemDisabler} />
+
+                                    <div className="btnDeleteTitle" onClick={() => {
+                                        let thisElem = document.querySelector('.btnDeleteTitle')
+                                        thisElem.parentNode.parentNode.remove()
+                                    }}>X</div>
+                                </div>
+                                <hr />
+                                <textarea
+                                    className="dvTitle dvDesc"
+                                    style={{
+                                        width: "100%",
+                                    }}
+                                    disabled={elemDisabler}
+                                    defaultValue={props.desc[key]}
+                                ></textarea>
+
+                                <hr />
+                                <div className="dvTitle dvDate">Added on: {props.date[key]}</div>
+                                <div className="col EditOrDeleteText">
+                                    <div className="btnEditTitle" onClick={() => {
+                                        let mainElem = document.getElementById(key)
+                                        if (elemDisabler === true) {
+                                            elemDisabler = false
+                                            mainElem.lastChild.firstChild.innerText = "Done";
+                                            mainElem.childNodes[0].querySelector(".dvTitle").disabled = false
+                                            mainElem.querySelector(".dvDesc").disabled = false
+                                        }
+                                        else if (elemDisabler === false) {
+                                            elemDisabler = true
+                                            mainElem.lastChild.firstChild.innerText = "Edit";
+                                            mainElem.childNodes[0].querySelector(".dvTitle").disabled = true
+                                            mainElem.querySelector(".dvDesc").disabled = true
+                                        }
+
+                                    }}>Edit</div>
+                                </div>
                             </div>
-                            <hr/>
-                            <div className="dvTitle">{props.desc[key]}</div>
-                               
-                            <hr/>
-                            <div className="dvTitle dvDate">Added on: {props.date[key]}</div>
-                            
-                            <div className="col EditOrDeleteText">
-                                <div className="btnEditTitle">Edit</div>
-                            </div>
-                        </div>
-                        <br/>
+                            <br />
                         </div>
                     )
                 }
@@ -44,7 +74,4 @@ const TodoList = (props) => {
         </div>
     )
 }
-// function DeleteSelected(key){
-//     console.log(key);
-// }
 export default TodoList
